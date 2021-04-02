4626b4b0866122f404e65eea067dfefd58c505ac
Weronika Rynkowska <43828905+weroryn330@users.noreply.github.com>
2021-04-02 23:28:43 +0200

    basics of object-oriented programming

--- Snake/script.js
+++ Snake/script.js
@@ -1,32 +1,30 @@
-const cheq = document.getElementById("chequered")
-const context = cheq.getContext('2d')
-
-window.onload = draw()
-
+const canvas = document.querySelector(".canvas");
+const context = canvas.getContext("2d");
 const box = 30;
 
-let snake = []
-snake[0] = {
-    x: 9 * box,
-    y: 10 * box
-}
+var snake;
 
-let food = {
-    x: Math.floor(Math.random() * 17 + 1) * box,
-    y: Math.floor(Math.random() * 17 + 1) * box
-}
-// 14:26
+(function setUp() {
+    snake = new Snake();
+    window.setInterval(() => {
+        snake.update();
+        snake.draw();
+    }, 500);
+}());
 
-let score = 0
+function Snake() {
+    this.x = 0;
+    this.y = 0;
+    this.xSpeed = box * 1;
+    this.ySpeed = 0;
 
-function draw() {
-    for (let i = 0; i < snake.length; i++) {
-        cheq.innerHTML = '<div class="head">frgrfefefegrgr</div>'
-        context.fillStyle = (i == 0) ? "green" : "white"
-        context.fillRect(snake[i].x, snake[i].y, box, box)
-        context.strokeStyle = "red";
-        context.strokeRext(snake[i].x, snake[i].y, box, box);
+    this.draw = function() {
+        context.fillStyle = "#8cffec";
+        context.fillRect(this.x, this.y, box, box);
     }
-    cheq.innerHTML = '<div class="head"></div>'
-    setTimeout("draw()", 1000);
+
+    this.update = function() {
+        this.x += this.xSpeed;
+        this.y += this.ySpeed;
+    }
 }
