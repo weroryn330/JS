4a4d53beac9754b28ce887e4a369b17b6a0b8a0a
Weronika Rynkowska <43828905+weroryn330@users.noreply.github.com>
2021-04-22 19:03:43 +0200

    Final Rain Effect

--- Pixels Effects/Rain/script.js
+++ Pixels Effects/Rain/script.js
@@ -18,36 +18,6 @@ rainImage.addEventListener('load', function () {
     /** number of particles to generate */
     const numberOfPatricles = 2000;
 
-    let mappedImage = [];
-
-    for (let y = 0; y < rainCanvas.height; y++) {
-        let row = [];
-        for (let x = 0; x < rainCanvas.width; x++) {
-            /* getting r, g and b from pixels.data array */
-            const r = pixels.data[(y * 4 * pixels.width) + (x * 4)]
-            const g = pixels.data[(y * 4 * pixels.width) + (x * 4 + 1)]
-            const b = pixels.data[(y * 4 * pixels.width) + (x * 4 + 2)]
-            /* pixel brightness based on r g b components */
-            const pixelBrightness = checkBrightness(r, g, b);
-            const cell = [
-                cellBr = pixelBrightness,
-            ];
-            row.push(cell);
-        }
-        mappedImage.push(row);
-    }
-
-    console.log(mappedImage);
-
-    /** Function checks the brightness of each pixel */
-    function checkBrightness(r, g, b) {
-        return Math.sqrt(
-            (r * r) * 0.299 +
-            (g * g) * 0.587 +
-            (b * b) * 0.114
-        ) / 100;
-    }
-
     /** Particle class */
     class Particle {
 
@@ -55,23 +25,13 @@ rainImage.addEventListener('load', function () {
         constructor() {
             this.x = Math.random() * rainCanvas.width;
             this.y = Math.random() * rainCanvas.height;
-            this.speed = 0;
-            this.position1 = Math.floor(this.y);
-            this.position2 = Math.floor(this.x);
             this.v = Math.random() * 5;
             this.size = Math.random() * 1.5 + 1;
         }
 
         /** Updates position of the particle (falling effect) */
         update() {
-            // helps us to get into correct info allocated by for loop (sth like iterator)
-            this.position1 = Math.floor(this.y);
-            this.position2 = Math.floor(this.x);
-            // [row] [cell] [celBr]
-            this.speed = mappedImage[this.position1][this.position2][0];
-            let movement = (2.5 - this.speed) + this.v;
-
-            this.y += movement;
+            this.y += this.v;
             if (this.y >= rainCanvas.height) {
                 this.y = 0;
                 this.x = Math.random() * rainCanvas.width;
